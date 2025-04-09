3169a68f019f797d9b428fe06b83536cb3d50568
georgegoldman <georgegoldmanjohn.o@gmail.com>
2025-04-09 20:05:17 +0100

    integrated sui to the bot

--- src/main.rs
+++ src/main.rs
@@ -7,15 +7,33 @@ use shuttle_runtime::SecretStore;
 use tracing::{error, info};
 use dotenv::dotenv;
 use std::env;
+use sui_sdk::SuiClientBuilder;
 
 struct Bot;
 
 #[async_trait]
 impl EventHandler for Bot {
     async fn message(&self, ctx: Context, msg: Message) {
+        
         if msg.content == "!hello" {
             if let Err(e) = msg.channel_id.say(&ctx.http, "world!").await {
                 error!("Error sending message: {:?}", e);
+            }
+        }
+
+        if msg.content == "!sui" {
+            match get_sui_testnet_version().await {
+                Ok(version) => {
+                    if let Err(e) = msg.channel_id.say(&ctx.http, format!("Sui testnet version: {}", version)).await {
+                        error!("Error sending message: {:?}", e);
+                    }
+                }
+                Err(e) => {
+                    error!("Error initializing sui client: {:?}", e);
+                    if let Err(e) = msg.channel_id.say(&ctx.http, "Failed to get sui client version").await {
+                        error!("Error sending message: {:?}", e);
+                    }
+                }
             }
         }
     }
@@ -23,6 +41,12 @@ impl EventHandler for Bot {
     async fn ready(&self, _: Context, ready: Ready) {
         info!("{} is connected!", ready.user.name);
     }
+}
+
+// async function to fetch sui testnet
+async fn get_sui_testnet_version() -> Result<String, anyhow::Error> {
+    let sui_testnet = SuiClientBuilder::default().build_testnet().await?;
+    Ok(sui_testnet.api_version().to_string())
 }
 
 #[shuttle_runtime::main]
